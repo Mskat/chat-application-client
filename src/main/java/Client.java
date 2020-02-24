13a7eb3c8c849c02bcff9056f1864edbd978d9c7
unknown <zur.katarzyna@gmail.com>
2020-02-24 11:12:13 +0000

    Add Runnable interface to handle threads
    Created ClientThread class implements the Runnable interface.
    There has been created an instance of Thread class.
    Input/output variables were changed to global variables.
    User can see what all other users typing.
    User can leave the chat by typing "exit".

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,7 +5,9 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    String name;
+    private String name = null;
+    private PrintWriter output = null;
+    private BufferedReader input = null;
 
     public static void main(String[] args) {
         String address = "127.0.0.1";
@@ -16,6 +18,7 @@ public class Client {
 
     private void Client(String address, int port) {
         BufferedReader inputLine = new BufferedReader(new InputStreamReader(System.in));
+
         try {
             System.out.print("Type your name: ");
             name = inputLine.readLine();
@@ -24,19 +27,56 @@ public class Client {
         }
 
         try {
-            Socket socket = new Socket(address, port);
-            System.out.println("Connected");
-            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Socket clientSocket = new Socket(address, port);
+            output = new PrintWriter(clientSocket.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Thread thread = new Thread(new ClientThread(clientSocket));
+            thread.start();
 
-            String inputMessage;
-            while(!(inputMessage = inputLine.readLine().toLowerCase()).equals("bye")) {
-                output.println(inputMessage);
-                System.out.println("Server received a message: " + input.readLine());
+            while (true) {
+                String message = inputLine.readLine();
+                if (!message.toLowerCase().equals("exit")) {
+                    output.println(name + ": " + message);
+                    output.flush();
+                } else {
+                    output.println(name + " left chat.");
+                    output.flush();
+                    output.close();
+                    inputLine.close();
+                    clientSocket.close();
+                }
             }
         } catch (IOException e) {
-            System.out.println("Server is not available");
-            //e.printStackTrace();
+            
+        }
+    }
+
+    class ClientThread implements Runnable {
+        Socket clientSocket;
+
+        private ClientThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            System.out.println("Connected. To leave the chat type \"exit\".");
+
+        }
+
+        @Override
+        public void run() {
+            output.println(name + " entered to conversation.");
+            output.flush();
+
+            try {
+                String message;
+                while ((message = input.readLine()) != null) {
+                    if (!message.startsWith(name)) {
+                        System.out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("You left chat.");
+                output.flush();
+                output.close();
+            }
         }
     }
 }
