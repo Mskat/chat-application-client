bd69560a114f73398584534f0ee32021e675b2ee
unknown <zur.katarzyna@gmail.com>
2020-03-20 19:50:29 +0000

    Add exception handling for main method

--- src/main/java/RunClient.java
+++ src/main/java/RunClient.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
+
 public class RunClient {
     private final static String address = "127.0.0.1";
     private final static int portNumber = 5000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Client client = new Client();
         client.startClient(address, portNumber);
     }
