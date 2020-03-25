469ab154e74be7c90e95cd5d7e715ab52998cce3
unknown <zur.katarzyna@gmail.com>
2020-03-25 21:01:50 +0000

    Add variable that specifies max number of clients

--- src/main/java/RunClient.java
+++ src/main/java/RunClient.java
@@ -3,9 +3,10 @@ import java.io.IOException;
 public class RunClient {
     private final static String address = "127.0.0.1";
     private final static int portNumber = 5000;
+    private final static int maxNumberOfClients = 10;
 
     public static void main(String[] args) throws IOException {
         Client client = new Client();
-        client.startClient(address, portNumber);
+        client.startClient(address, portNumber, maxNumberOfClients);
     }
 }
