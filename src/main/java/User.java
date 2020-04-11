827e6fc0c566e1c6bd5964a3012951cb80bcbf01
unknown <zur.katarzyna@gmail.com>
2020-04-11 18:28:43 +0100

    Move code that supports name variable to User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,7 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class User {
+    private static String name;
     private static BufferedReader lineTypedFromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getNameFromUser() throws IOException {
+        System.out.print("Type your name: ");
+        return setName();
+    }
+
+    private static String setName() throws IOException {
+        name = getUserInput().toUpperCase();
+        return name;
+    }
 
     public static String getUserInput() throws IOException {
         return lineTypedFromKeyboard.readLine();
