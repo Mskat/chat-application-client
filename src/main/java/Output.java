53a0c546fee8d0725f40e30cf710254678469b54
unknown <zur.katarzyna@gmail.com>
2020-04-11 14:34:53 +0100

    Create Output class, transfer code from Client class

---
+++ src/main/java/Output.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Output {
+    private static PrintWriter output = null;
+
+    public Output(Socket socket) throws IOException {
+        output = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public static void userJoinTheChat(String name) {
+        output.println(name + " entered to conversation.");
+    }
+
+    public static void userLeftTheChat(String name) {
+        output.println(name + " left chat.");
+    }
+
+    public static void closeOutput() {
+        output.close();
+    }
+
+    public static void printMessage(String name, String message) {
+        output.println(name + ": " + message);
+    }
+}
