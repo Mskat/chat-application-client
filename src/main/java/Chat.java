0ed20ac647f13a96fffbfe27367840b549e01403
unknown <zur.katarzyna@gmail.com>
2020-04-12 22:40:43 +0100

    Rename class from RunClient to Chat

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
 
-public class RunClient {
+public class Chat {
     private final static String address = "127.0.0.1";
     private final static int portNumber = 5000;
     private final static int maxNumberOfClients = 10;
 
     public static void main(String[] args) throws IOException {
-        Client client = new Client();
-        client.startClient(address, portNumber, maxNumberOfClients);
+        Connection connection = new Connection();
+        connection.start(address, portNumber, maxNumberOfClients);
     }
 }
